609007d007fa590dbc4d72003629a70d1fddd8fc
Jasen LaBolle <jasenlabolle@gmail.com>
2023-01-17 19:51:23 -0600

    Add project color profile

---
+++ lib/helpers/my_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  MyColors._();
+
+  static const Color primary_neon = Color(0xFF42F1EA);
+  static const Color primary_light = Color(0xFF28B2AC);
+  static const Color primary = Color(0xFF287E7B);
+  static const Color primary_dark = Color(0xFF2B6260);
+  static const Color primary_dull = Color(0xFF455E5D);
+  static const Color primary_grey = Color(0xFF4F5656);
+
+  static const Color secondary_neon = Color(0xFF5587F3);
+  static const Color secondary_light = Color(0xFF3A64BE);
+  static const Color secondary = Color(0xFF35518B);
+  static const Color secondary_dark = Color(0xFF35476C);
+  static const Color secondary_dull = Color(0xFF4F5768);
+  static const Color secondary_grey = Color(0xFF585A5F);
+
+  static const Color highlight_neon = Color(0xFF4DF844);
+  static const Color highlight_light = Color(0xFF39DA31);
+  static const Color highlight = Color(0xFF3AA835);
+  static const Color highlight_dark = Color(0xFF3D8339);
+  static const Color highlight_dull = Color(0xFF5E7E5C);
+  static const Color highlight_grey = Color(0xFF6A7369);
+
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
