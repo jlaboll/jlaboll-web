3a7e2936ac22aa8c3ab2afc6b75d3a066ef74f02
Jasen LaBolle <jasenlabolle@gmail.com>
2023-01-17 19:56:29 -0600

    Create basic homepage

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:jlaboll_web/helpers/constants.dart';
+import 'package:jlaboll_web/helpers/my_colors.dart';
 
-import '../helpers/constants/object_constants.dart';
+import '../helpers/helper_methods.dart';
+import '../helpers/responsive_layout.dart';
 
 class Home extends StatelessWidget {
   final ScrollController scrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
-    return pageBody(context, child: Column());
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      width: double.infinity,
+      child: padding(context,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                homeTitle1,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: ResponsiveLayout.isSmallScreen(context) ? 15 : 20,
+                    fontWeight: FontWeight.bold,
+                    color: MyColors.primary_neon),
+              ),
+              VertScaledBox(context, 0.01),
+              Text(
+                homeTitle2,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: ResponsiveLayout.isSmallScreen(context) ? 25 : 30,
+                    fontWeight: FontWeight.bold,
+                    color: MyColors.white),
+              ),
+              VertScaledBox(context, 0.01),
+              Text(
+                homeTitle3,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: ResponsiveLayout.isSmallScreen(context) ? 20 : 25,
+                    fontWeight: FontWeight.bold,
+                    color: MyColors.white),
+              ),
+              VertScaledBox(context, 0.01),
+              CircleAvatar(
+                  backgroundColor: MyColors.secondary,
+                  radius: 120,
+                  child: CircleAvatar(
+                    radius: 110,
+                    foregroundImage: AssetImage('profile.jpg'),
+                    backgroundImage: AssetImage('spinner.gif'),
+                  )),
+            ],
+          )),
+    );
   }
 }
